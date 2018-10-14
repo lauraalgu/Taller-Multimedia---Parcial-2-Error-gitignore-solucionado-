8cbf34f5d9babe1c4e3f97cb10c3f4cf9813355c
lauraalgu <lauraalgu@unisabana.edu.co>
2018-10-14 09:15:58 -0500

    Taller Multimedia - Parcial 2 (Error gitignore solucionado)

---
+++ TableTennisMatch/Assets/Scenes/Scripts/Left.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.left * 0.5f);
+        }
+
+        //Motion Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.right * 0.5f);
+        }
+
+        //Up
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.up * 0.5f);
+        }
+
+        //Down
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.down * 0.5f);
+        }
+
+        //Forward
+        if (Input.GetKey(KeyCode.Q))
+        {
+            this.transform.Translate(Vector3.left * 0.5f);
+        }
+
+        //Back
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.transform.Translate(Vector3.right * 0.5f);
+        }
+
+
+    }
+}
---
+++ TableTennisMatch/Assets/Scenes/Scripts/Rigth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.left * 0.5f);
+        }
+
+        //Motion Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.right * 0.5f);
+        }
+
+        //Up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * 0.5f);
+        }
+
+        //Down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * 0.5f);
+        }
+
+
+    }
+}
